86365f702f0e65ac6260a245217b9a8fa3e4ab05
AbrahamOh <gen_a@live.com>
2024-02-11 19:14:07 +1100

    components such as data_ingestion.py

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,82 @@
+import os
+import sys
+from src.exception import CustomException # Calling from src folder
+from src.logger import logging # Importing logging from src.logger
+import pandas as pd
+
+# Scikit-Learn's train_test_split module
+from sklearn.model_selection import train_test_split
+
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
+
+# dataclasses from Python
+from dataclasses import dataclass
+
+# DataIngestConfig Class defining inputs eg. where I'm saving the data.
+# That's the classes. You'll see with the artifacts.
+# @dataclass decorator. inside a class you use init. 
+# but if you use @dataclass you can directly define your class variable.
+# alternative: create a folder 'config' in components folder.
+# and then define the class over there. 
+# but DataIngestion can refer to DataIngestionConfig here. 
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
+
+# If we want to go beyond defining variables, but have functions:
+# We'll have to use class, not dataclass.
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig() # Will have the 3 values above.
+
+    # Creating own function. 
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+        try:
+            # Reading locally, but could be database.
+            # eg. mongodb_client / sql_client in utils.py
+            df=pd.read_csv('notebook\data\stud.csv')
+
+            # Keep writing logs
+            logging.info('Read the dataset as dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info("Train test split initiated")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info("Inmgestion of the data iss completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
